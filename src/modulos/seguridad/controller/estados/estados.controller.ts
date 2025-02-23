d8f9af26c0bd80133c7b7193b3eb73ebcd8fd015
cesar_cz <yo@cesarcancino.com>
2025-02-23 13:15:08 -0300

    probando interfaces

--- src/modulos/seguridad/controller/estados/estados.controller.ts
+++ src/modulos/seguridad/controller/estados/estados.controller.ts
@@ -1,41 +1,43 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
-import { EstadoDto } from '../../dto/estado.dto'; 
+import { EstadoDto } from '../../dto/estado.dto';
 import { EstadosService } from '../../servicios/estados/estados.service';
+import { EstadosInterface } from 'src/interfaces/estados.interface';
 
 @Controller('estados')
 export class EstadosController {
 
-    constructor(private estadosService: EstadosService){}
+    constructor(private estadosService: EstadosService) { }
 
     @Get()
     @HttpCode(HttpStatus.OK)
-    index()
-    {
-        return this.estadosService.getDatos();
-    }   
-
-    @Get(':id') 
-    @HttpCode(HttpStatus.OK)
-    show(@Param() params): {} { 
-    return this.estadosService.getDato(params.id);
+    async index():Promise<EstadosInterface[]> {
+        let datos=await this.estadosService.getDatos();
+        return datos.map(item => ({
+            id: item.id,
+            nombre: item.nombre
+          }));
     }
-    
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    async show(@Param() params):Promise<EstadosInterface>   {
+        let datos = await this.estadosService.getDato(params.id);
+        return {id:datos.id, nombre: datos.nombre}
+    }
+
     @Post()
     @HttpCode(HttpStatus.CREATED)
-    create(@Body() dto: EstadoDto)
-    {
+    create(@Body() dto: EstadoDto) {
         return this.estadosService.addDatos(dto);
     }
 
     @Put(':id')
-    update(@Param() params, @Body() dto: EstadoDto)
-    {
-        this.estadosService.updateDatos(params.id, {nombre: dto.nombre});
-        return {estado: "ok", mensaje: "Se modificó el registro exitosamente"}
+    update(@Param() params, @Body() dto: EstadoDto) {
+        this.estadosService.updateDatos(params.id, { nombre: dto.nombre });
+        return { estado: "ok", mensaje: "Se modificó el registro exitosamente" }
     }
     @Delete(':id')
-    destroy(@Param() params)
-    {
+    destroy(@Param() params) {
         return this.estadosService.deleteDato(params.id);
     }
 }
--- src/modulos/seguridad/servicios/estados/estados.service.ts
+++ src/modulos/seguridad/servicios/estados/estados.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EstadosInterface } from 'src/interfaces/estados.interface';
 import { Estado } from 'src/modulos/db_type_orm/modelos/estados.entity';
 import { EstadoDto } from 'src/modulos/seguridad/dto/estado.dto';
 import { Repository } from 'typeorm';
@@ -10,44 +11,38 @@ export class EstadosService {
     constructor(
         @InjectRepository(Estado)
         private repositorio: Repository<Estado>
-    ){}
+    ) { }
 
-    async getDatos():Promise<Estado[]>
-    {
-        return await this.repositorio.find(
-            {
-                order:
-                {
-                    id:"desc"
-                }
-            });
+    async getDatos(): Promise<Estado[]> {
+        return await this.repositorio.find({
+            order: {
+                id: "desc"
+            }
+        });
     }
-    async getDato(id: number): Promise<Estado>
-    {
+
+    async getDato(id: number): Promise<Estado> {
         let datos = await this.repositorio.findOne(
             {
                 where:
                 {
-                    id:id
+                    id: id
                 }
             });
-        if(!datos)
-        {
+        if (!datos) {
             throw new HttpException(
                 {
                     estado: HttpStatus.BAD_REQUEST,
                     mensaje: 'El registro no existe en el sistema'
                 }, HttpStatus.BAD_REQUEST, {
-                    cause: {name:"", message:""}
-                });
-        }else
-        {
+                cause: { name: "", message: "" }
+            });
+        } else {
             return datos;
         }
     }
 
-    async addDatos(dto: EstadoDto)
-    {
+    async addDatos(dto: EstadoDto) {
         let existe = await this.repositorio.findOne(
             {
                 where:
@@ -55,48 +50,42 @@ export class EstadosService {
                     nombre: dto.nombre
                 }
             });
-        if(existe)
-        {
+        if (existe) {
             throw new HttpException(`El registro ${dto.nombre} ya existe en el sistema`, HttpStatus.BAD_REQUEST);
-            
-        }else
-        {
+
+        } else {
             try {
                 let save = this.repositorio.create(dto);
                 this.repositorio.save(save);
-                return {estado: 'ok', mensaje:'Se crea el registro exitosamente'}
+                return { estado: 'ok', mensaje: 'Se crea el registro exitosamente' }
             } catch (error) {
                 throw new HttpException(`Ocurrió un error inesperado`, HttpStatus.BAD_REQUEST);
             }
-            
+
         }
     }
-    async updateDatos(id:number, dto: EstadoDto)
-    {
+    async updateDatos(id: number, dto: EstadoDto) {
         let datos = await this.repositorio.findOne(
             {
                 where:
                 {
-                    id:id
+                    id: id
                 }
             });
-        if(!datos)
-        {
+        if (!datos) {
             throw new HttpException(
                 {
                     estado: HttpStatus.BAD_REQUEST,
                     mensaje: 'El registro no existe en el sistema'
                 }, HttpStatus.BAD_REQUEST, {
-                    cause: {name:"", message:""}
-                });
-        }else
-        {
-            await this.repositorio.update( {id}, dto);
+                cause: { name: "", message: "" }
+            });
+        } else {
+            await this.repositorio.update({ id }, dto);
         }
-        
+
     }
-    async deleteDato(id: number)
-    {
+    async deleteDato(id: number) {
         /*let existe = await this.repositorio_producto.find(
             {
                 where:
